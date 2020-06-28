06f6a3c1df4cf35ae0dab3401952c4ae64863fae
martoss89 <66514902+martoss89@users.noreply.github.com>
2020-06-28 02:27:54 +0200

    Create kolka.py

---
+++ kolka.py
@@ -0,0 +1,67 @@
+import pyautogui, time
+
+time.sleep (5)
+
+pyautogui.click () # click to make window active
+
+pyautogui.click(x=460, y=57)  # move to 460, 57, then click the left mouse button
+pyautogui.moveTo(x=300, y=450)
+pyautogui.dragTo(x=600, y=730)
+pyautogui.mouseUp()
+pyautogui.click(x=650, y=735)
+
+pyautogui.moveTo(x=450, y=400)
+pyautogui.dragTo(x=545, y=500)
+pyautogui.click(x=550, y=525)
+
+pyautogui.moveTo(x=505, y=450)
+pyautogui.dragTo(x=605, y=540)
+pyautogui.click(x=610, y=590)
+
+pyautogui.moveTo(x=560, y=500)
+pyautogui.dragTo(x=660, y=590)
+pyautogui.click(x=700, y=615)
+
+pyautogui.moveTo(x=550, y=550)
+pyautogui.dragTo(x=650, y=650)
+pyautogui.click(x=900, y=730)
+
+pyautogui.moveTo(x=525, y=600)
+pyautogui.dragTo(x=640, y=700)
+pyautogui.click(x=900, y=730)
+
+pyautogui.moveTo(x=500, y=650)
+pyautogui.dragTo(x=620, y=740)
+pyautogui.click(x=900, y=730)
+
+pyautogui.moveTo(x=440, y=670)
+pyautogui.dragTo(x=540, y=780)
+pyautogui.click(x=900, y=730)
+
+pyautogui.moveTo(x=370, y=670)
+pyautogui.dragTo(x=470, y=770)
+pyautogui.click(x=900, y=730)
+
+pyautogui.moveTo(x=300, y=650)
+pyautogui.dragTo(x=400, y=750)
+pyautogui.click(x=750, y=730)
+
+pyautogui.moveTo(x=265, y=615)
+pyautogui.dragTo(x=365, y=715)
+pyautogui.click(x=750, y=730)
+
+pyautogui.moveTo(x=245, y=550)
+pyautogui.dragTo(x=345, y=650)
+pyautogui.click(x=750, y=730)
+
+pyautogui.moveTo(x=255, y=500)
+pyautogui.dragTo(x=355, y=600)
+pyautogui.click(x=750, y=730)
+
+pyautogui.moveTo(x=275, y=425)
+pyautogui.dragTo(x=375, y=525)
+pyautogui.click(x=750, y=730)
+
+pyautogui.moveTo(x=365, y=415)
+pyautogui.dragTo(x=465, y=515)
+pyautogui.click(x=750, y=730)# ZUT-ZiIP
